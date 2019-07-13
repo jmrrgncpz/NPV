e2e04cf262aa38b01210da7e9e304e15d7b58982
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 16:15:27 +0800

    -add cashflow domain class

---
+++ NPV/Models/Domain/Cashflow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NPV.Models.Domain
+{
+    public class Cashflow
+    {
+        public int ID { get; set; }
+        public int ParametersID { get; set; }
+        public decimal Value { get; set; }
+    }
+}
