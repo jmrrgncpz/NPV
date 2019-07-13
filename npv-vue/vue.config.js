1bb3666909f4d5afe1135bb57820f559f123db30
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 15:55:49 +0800

    -install bulma -install axios -setup dev proxy

--- npv-vue/vue.config.js
+++ npv-vue/vue.config.js
@@ -1,3 +1,11 @@
 modules.exports = {
-    
+    devServer : {
+        proxy : {
+            '^api' : {
+                target : "http://localhost:56917",
+                ws : true,
+                changeOrigin : true
+            }
+        }
+    }
 }
