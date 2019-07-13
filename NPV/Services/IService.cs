0b24bbc90deb08f0c361ef5a086eab7fbd80a197
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 21:22:16 +0800

    implemented calculation of present value

---
+++ NPV/Services/IService.cs
@@ -0,0 +1,14 @@
+﻿using NPV.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NPV.Services
+{
+    public interface IService
+    {
+        IEnumerable<NPVCalculationsVM> GetHistory();
+        NPVCalculationsVM Calculate(ParametersVM parameters);
+    }
+}
---
+++ NPV/Services/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NPV.Models.Domain;
+using NPV.Models.View;
+
+namespace NPV.Services
+{
+    public class Service : IService
+    {
+        public NPVCalculationsVM Calculate(ParametersVM parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NPVCalculationsVM> GetHistory()
+        {
+            throw new NotImplementedException();
+        }
+
+        private decimal CalculatePV(decimal Cashflow, decimal DiscountRate, int Year)
+        {
+            decimal denominator = (decimal)Math.Pow((double)(1 + (DiscountRate * 0.01m)), Year);
+            return Cashflow / denominator;
+        }
+
+        private NPVCalculation CalculateNPV()
+        {
+
+        }
+    }
+}
