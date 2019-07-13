8b126ec6996b641342af7b3946ae4f86660d67e5
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 21:53:59 +0800

    -add save method to IService

--- NPV/Services/IService.cs
+++ NPV/Services/IService.cs
@@ -12,5 +12,6 @@ namespace NPV.Services
     {
         IEnumerable<NPVCalculationsVM> GetHistory();
         IEnumerable<BaseNPVCalculation> ProcessCalculation(ParametersVM parameters);
+        NPVCalculationsVM SaveCalculation(ParametersVM parameters, IEnumerable<BaseNPVCalculation> NPVs);
     }
 }
