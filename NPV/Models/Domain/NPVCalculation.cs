9cd4a65b55b87a9a8c4a8204bac6dd86d83d7fd4
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 16:13:42 +0800

    add npvcalculation (single npv)

---
+++ NPV/Models/Domain/NPVCalculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NPV.Models.Domain
+{
+    public class NPVCalculation
+    {
+        public int ID { get; set; }
+        public int NPVSeriesCalculationID { get; set; }
+        public decimal NPV { get; set; }
+        public decimal DiscountRate { get; set; }
+    }
+}
