0ce16b679db6bed8893e18e064fef91a0fedef1b
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 17:06:27 +0800

    -add dbsets -add constants class -add constant string for connection string

---
+++ NPV/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NPV
+{
+    public class Constants
+    {
+        public const string DBContextName = "NPVContent";
+    }
+}
--- NPV/DAL/NPVContext.cs
+++ NPV/DAL/NPVContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NPV.Models.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,17 @@ namespace NPV.DAL
 {
     public class NPVContext : DbContext
     {
+        public NPVContext() : base(GetOptions(Constants.DBContextName)) { }
+
+        private static DbContextOptions GetOptions(string connString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connString).Options;
+        }
+
+        public DbSet<NPVCalculations> NPVCalculations { get; set; }
+        public DbSet<NPVCalculation> NPVCalculation { get; set; }
+        public DbSet<Cashflow> Cashflow { get; set; }
+        public DbSet<Parameters> Parameters { get; set; }
     }
+
 }
