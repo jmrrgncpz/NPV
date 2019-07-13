0901bbfc3ff442900e3773ae01ff528fe922ba05
jmrrgncpz <paz.jesr@gmail.com>
2019-07-13 16:11:28 +0800

    -add paratemers view model

---
+++ NPV/Models/View/ParametersVM.cs
@@ -0,0 +1,12 @@
+﻿using NPV.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NPV.Models.View
+{
+    public class ParametersVM : BaseParameters
+    {
+    }
+}
