99b47428f4fe16f9fcbd3156a624597da2f9c9ca
jmrrgncpz <paz.jesr@gmail.com>
2019-07-15 01:45:05 +0800

    -add cashflow input (forreal)

---
+++ npv-vue/src/components/CashflowInput.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="cashflow-input box">
+        <span class="cashflow-number">
+            {{ number }}
+        </span>
+        <input class="input" type="number" v-bind:value="value" v-on:input="$emit('update:value', $event.target.value)">
+        <button class="button" v-on:click="$emit('cashflow-input-remove-btn-clicked')">
+            <b-icon pack="fas" icon="times" size="is-small"></b-icon>
+        </button>
+    </div>
+</template>
+
+<script>
+const props = {
+    number : {
+        type : Number,
+        required : true
+    },
+    value : {
+        type : [Number, String],
+        required : true
+    }
+}
+
+export default {
+    props,
+}
+</script>
+
+<style scoped>
+    .cashflow-input{
+        padding: 0.25em;
+        display: flex;
+        align-items: center;
+    }
+
+    .cashflow-input .input{
+        flex: 1;
+    }
+
+    .cashflow-number{
+        padding: 0.5em;
+    }
+</style>
