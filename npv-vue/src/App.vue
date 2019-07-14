62afbcdd469256c67f7a7b24bf6fa9564b7ea29e
jmrrgncpz <paz.jesr@gmail.com>
2019-07-14 13:41:21 +0800

    -install fontawesome -import buefy -import fontawesome -import axios

--- npv-vue/src/App.vue
+++ npv-vue/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    
+    <main>
+      
+    </main>
   </div>
 </template>
 
@@ -14,4 +16,21 @@ export default {
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin : 0;
+}
+
+html,
+body,
+#app{
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+#app{
+}
 </style>
--- npv-vue/src/main.js
+++ npv-vue/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios";
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
 
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = axios;
+Vue.use(Buefy);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
